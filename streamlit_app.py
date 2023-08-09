4f64d9a9854381bf12969008ddb8554361f7ce38
kj777 <49434658+kj777@users.noreply.github.com>
2023-08-09 15:31:06 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
-streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']) 
+fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']) 
+fruits_to_show = my_fruit_list.loc[fruit_selected]
 
 # ページにテーブルを表示します。
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
