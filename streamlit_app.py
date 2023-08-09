6fee8cf319a87b5b5e89f9cb51c60e969feb7856
kj777 <49434658+kj777@users.noreply.github.com>
2023-08-09 15:32:07 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # ここに選択リストを置き、含めたい果物を選択できるようにしましょう。
 fruits_selected = streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']) 
-fruits_to_show = my_fruit_list.loc[fruit_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # ページにテーブルを表示します。
 streamlit.dataframe(fruits_to_show)
