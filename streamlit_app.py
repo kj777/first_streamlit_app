adc2c73f4fa7ac580ceda719c49dbbc9f3d0fc09
kj777 <49434658+kj777@users.noreply.github.com>
2023-08-13 10:31:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,10 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruit load list contaians:")
-streamlit.text(my_data_row)
+#streamlit.text("The fruit load list contaians:")
+streamlit.header("The fruit load list contaians:")
+#streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 
 
