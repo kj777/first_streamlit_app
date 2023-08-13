442f52d5cef80a5e23af79da7ad924f84f21e556
kj777 <49434658+kj777@users.noreply.github.com>
2023-08-13 11:33:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,6 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normallized
 
-
-
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -39,48 +37,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    #streamlit.dataframe(fruityvice_normallized)
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
-      
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
-
-
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text("The fruit load list contaians:")
-streamlit.header("The fruit load list contaians:")
-#streamlit.text(my_data_row)
-streamlit.dataframe(my_data_rows)
-
-fruit_choice = streamlit.text_input('What fruit would you like to add ?', 'jackfruit')
-streamlit.write('Tahnks for adding ', fruit_choice)
-
-#streamlit.write('Thanks for adding ',add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
-
